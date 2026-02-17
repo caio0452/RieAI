8f4225d53e1330a00ff4daa4ef1510035f9baef4
Caio P. <caio@example.com>
2026-02-17 15:51:02 -0300

    Fix converting JSON to string

--- commands/fal/fal_common.py
+++ commands/fal/fal_common.py
@@ -88,7 +88,7 @@ class BaseFalCommand(commands.Cog):
 
         except httpx.HTTPStatusError as e:
             traceback.print_exc()
-            trimmed_err_json_str = e.response.json().dumps()[:1800]
+            trimmed_err_json_str = json.dumps(e.response.json(), indent=2)[:1800]
             await interaction.followup.send(f":x: HTTP error {e.response.status_code} while generating content. ```json\n{trimmed_err_json_str}```")
 
         except Exception as e:
